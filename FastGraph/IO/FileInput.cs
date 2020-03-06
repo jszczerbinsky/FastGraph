9800be6b7164c0a1c35d7a8cc5a48351b4e190c6
ChickenMan4236 <j.bek.s212@gmail.com>
2020-03-06 10:47:48 +0100

    FileInputException

--- FastGraph/IO/FileInput.cs
+++ FastGraph/IO/FileInput.cs
@@ -9,49 +9,55 @@ namespace FastGraph.IO
 
         public void LoadCSV(GraphNode loadTo, string path, bool ignoreFirstLine, int xColumntID, int yColumnID, char separator)
         {
-            FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read);
-            StreamReader sr = new StreamReader(fs);
-
-            string text = sr.ReadToEnd();
-            int lines = 0;
-
-            foreach (char c in text)
-                if (c == '\n')
-                    lines++;
-
-            sr.BaseStream.Position = 0;
-            sr.DiscardBufferedData();
-
-            string xColBuff;
-            string yColBuff;
-
-            for(int i = 0; i < lines; i++)
+            try
             {
-                if (ignoreFirstLine && i == 0) continue;
+                FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+                StreamReader sr = new StreamReader(fs);
 
-                xColBuff = "";
-                yColBuff = "";
+                string text = sr.ReadToEnd();
+                int lines = 0;
 
-                string line = sr.ReadLine();
-                int actualCol = 1;
+                foreach (char c in text)
+                    if (c == '\n')
+                        lines++;
 
-                foreach(char c in line)
+                sr.BaseStream.Position = 0;
+                sr.DiscardBufferedData();
+
+                string xColBuff;
+                string yColBuff;
+
+                for (int i = 0; i < lines; i++)
                 {
-                    if (c == separator) { actualCol++; continue; }
-                    if (actualCol == xColumntID)
-                        xColBuff += c;
-                    if (actualCol == yColumnID)
-                        yColBuff += c;
+                    if (ignoreFirstLine && i == 0) continue;
+
+                    xColBuff = "";
+                    yColBuff = "";
+
+                    string line = sr.ReadLine();
+                    int actualCol = 1;
+
+                    foreach (char c in line)
+                    {
+                        if (c == separator) { actualCol++; continue; }
+                        if (actualCol == xColumntID)
+                            xColBuff += c;
+                        if (actualCol == yColumnID)
+                            yColBuff += c;
+                    }
+
+                    float x = float.Parse(xColBuff);
+                    float y = float.Parse(yColBuff);
+
+                    loadTo.Values.Add(new Coordinate(x, y));
                 }
 
-                float x = float.Parse(xColBuff);
-                float y = float.Parse(yColBuff);
-
-                loadTo.Values.Add(new Coordinate(x, y));
+                sr.Close();
+                fs.Close();
+            }catch(Exception e)
+            {
+                throw new FileInputException("Incorrect file input", e);
             }
-
-            sr.Close();
-            fs.Close();
         }
     }
 }
---
+++ FastGraph/IO/FileInputException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace FastGraph.IO
+{
+    public class FileInputException : Exception
+    {
+        public FileInputException(string message) : base(message)
+        {
+        }
+
+        public FileInputException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected FileInputException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
